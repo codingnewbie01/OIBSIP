1d5ad19ec15dde8234602f2285d692b55b0cc8b1
Arvind Verma <hey.arvind02@gmail.com>
2023-09-12 11:42:29 +0530

    add some js logic

---
+++ Temp_Converter/temp_convert.js
@@ -0,0 +1,22 @@
+const u1=document.querySelector(".unit1");
+const d1=document.querySelector(".deg1");
+const u2=document.querySelector(".unit2");
+const d2=document.querySelector(".deg2");
+
+var t;
+const a= d1.addEventListener('input', function b(){
+    let t1 =d1.value;
+    // let ft=(t1*9/5)+32;
+    // d2.value=ft;
+    return t1;
+})
+console.log(a.b);
+
+// u1.addEventListener('input', function(){
+//     let tu1 =u1.value;
+//     console.log(tu1);
+// })
+// u2.addEventListener('input', function(){
+//     let tu2 =u2.value;
+//     console.log(tu2);
+// })
