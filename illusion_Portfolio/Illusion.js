4d8bfff3e9b39da79352d6fed1fc943c0abac79b
Arvind Verma <hey.arvind02@gmail.com>
2023-09-20 22:19:04 +0530

    Started Working on porfolio

---
+++ illusion_Portfolio/Illusion.js
@@ -0,0 +1,13 @@
+// var a = document.querySelector("#navbar")
+// a.innerHTML="Link is not possible";
+// console.log(a);
+console.log(document.querySelector(a));
+
+// a.addEventListener('click', function(){
+//     if(document.querySelector(a).innerHTML=="About Me"){
+//         alert("About me clicked")
+//     }
+//     else
+//         alert("Other clicked")
+    
+// })
